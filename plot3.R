e13faa80a48e4fab0e5e796835f3d3fd58d6d681
Zach <zach160@gmail.com>
2018-06-25 18:31:31 -0600

    plot 3

--- plot3.R
+++ plot3.R
@@ -1,11 +1,10 @@
 library(ggplot2)
 baltimoreNEI <- NEI[fips=="24510",]
 
-png("plot3.png")
-
 g <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type))+ geom_bar(stat="identity") + 
     facet_grid(.~type,scales = "free",space="free") +
     labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) +
     labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+ggsave(file="plot3.png")
 
 dev.off()
