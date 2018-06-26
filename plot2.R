c484ae65a4aa8d276350afafedaa4253d0f1c9f7
Zach <zach160@gmail.com>
2018-06-25 18:26:38 -0600

    plots 1-3

---
+++ plot2.R
@@ -0,0 +1,9 @@
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[fips=="24510", lapply(.SD, sum, na.rm=TRUE), .SDcols=c("Emissions"), by=year]
+
+png(filename = "plot2.png")
+barplot(totalNEI[,Emissions], names=totalNEI[,year], xlab="Years", 
+        ylab="Emissions", main="Emissions Over the Years in Balitmore" )
+
+dev.off()
